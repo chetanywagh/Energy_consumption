c572b14299f3acf997b4eceb0dd5e94f437802da
Chetan wagh <150357946+chetanywagh@users.noreply.github.com>
2025-08-07 00:00:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -196,9 +196,9 @@ avg_val = np.mean(latest)
 
 st.markdown("###  Forecast Summary")
 col1, col2, col3 = st.columns(3)
-col1.metric(" Max Forecast", f"{max_val:.2f} MW")
-col2.metric(" Min Forecast", f"{min_val:.2f} MW")
-col3.metric(" Avg Forecast", f"{avg_val:.2f} MW")
+col1.metric(" 🔺 Max Forecast", f"{max_val:.2f} MW")
+col2.metric(" 🔻 Min Forecast", f"{min_val:.2f} MW")
+col3.metric(" 🔻 Avg Forecast", f"{avg_val:.2f} MW")
 
 # ------------------------- Forecast Table -------------------------
 st.subheader(f" Forecast Table - {future_days} Day(s)")
@@ -211,6 +211,7 @@ st.download_button(
     file_name="daily_energy_forecast.csv",
     mime="text/csv"
 )
+
 
 
 
